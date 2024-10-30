f6de3b8379b2f4e2898136cf58aa7a6272cf3f17
dries.k <k.dries@protonmail.com>
2024-10-30 17:55:54 +0100

    utils: fix typo

--- src/utils.h
+++ src/utils.h
@@ -1,21 +1,37 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-
 #include <QWidget>
 
 #ifdef USE_KDE
 #include <KNotification>
 #endif
 
-QString getTheme();
-void updateWidgetProperty(QWidget * widget, const char *name, const QVariant &value);
-QString readFile(QString fileName);
-void writeFile(QString fileName, QByteArray data);
-QString extractImage(QString text);
-QString getUuid();
-bool isImage(const QString& fileName);
-qint64 dirSize(const QString& dirName);
+namespace Utils {
+
+QString
+getTheme();
+
+void
+updateWidgetProperty(QWidget* widget, const char* name, const QVariant& value);
+
+QString
+readFile(QString fileName);
+
+void
+writeFile(QString fileName, QByteArray data);
+
+QString
+extractImage(QString text);
+
+QString
+getUuid();
+
+bool
+isImage(const QString& fileName);
+
+qint64
+dirSize(const QString& dirName);
 
 #ifdef USE_KDE
 KNotification::Urgency
